f1e8e84f1ab876a7c12bfaa3a6dcf64a72ad1db9
apenasampedro <apenasampedro@eniac26.danielcastelao.org>
2014-12-04 09:49:35 +0100

    Commit blablabla.2

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -18,7 +18,8 @@ if (metodoMio(x))
 System.out.print(x + " ");                         	 
 }         	 
 	}
-    //al finalizar el metodoMIo la variable j=3 y la aux=false
+    //1-al finalizar el metodoMIo la variable=16 j=3 y la aux=false
+    //2-al finalizar el metodoMio la variable=22 y j=22 y la aux=true
 public static boolean metodoMio(int variable){
 int j = 2;
 boolean aux=true;
