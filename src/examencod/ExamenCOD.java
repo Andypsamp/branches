5f0f920f47062bbd18b99cf935c1fb2ffe55c8a7
apenasampedro <apenasampedro@eniac26.danielcastelao.org>
2014-12-04 09:06:33 +0100

    Hice el formateo del codigo y le cambie el nombre al metodo

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -1,29 +1,32 @@
-
 package examencod;
 
 public class ExamenCOD {
 
     public static void main(String[] args) {
- int iN = 11;
-            	if (metodoMio(iN))
-System.out.println("Es numero primo");
-else
- System.out.println("NO es numero primo");           	 
-          	 
-for (int x=2;x<1000;x++) {
-if (metodoMio(x))
-System.out.print(x + " ");                         	 
-}         	 
-	}
-public static boolean metodoMio(int variable){
-int k = 2;
-boolean aux=true;
-while ((aux) && (k!=variable)){
-if (variable % k == 0)
-aux = false;
-k++;
-}
-return aux;
-  } 	 
-    
+        int iN = 11;
+        if (metodoTuyo(iN)) {
+            System.out.println("Es numero primo");
+        } else {
+            System.out.println("NO es numero primo");
+        }
+
+        for (int x = 2; x < 1000; x++) {
+            if (metodoTuyo(x)) {
+                System.out.print(x + " ");
+            }
+        }
+    }
+
+    public static boolean metodoTuyo(int variable) {
+        int k = 2;
+        boolean aux = true;
+        while ((aux) && (k != variable)) {
+            if (variable % k == 0) {
+                aux = false;
+            }
+            k++;
+        }
+        return aux;
+    }
+
 }
